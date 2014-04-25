a1fe1c38c2f30a66f9969a5e211995851fb9d397
mmcdon20 <mmcdon20@live.com>
2014-04-24 21:54:27 -0500

    added solution to problem 24

--- src/main/scala/Abbey024.scala
+++ src/main/scala/Abbey024.scala
@@ -1,5 +1,20 @@
 object Abbey024 extends App {
-  def reverse(text: String): String =
-    if (text.isEmpty) "" else reverse(text.tail) :+ text.head
-  println(reverse(readLine()))
+  val n = readInt()
+  val numbers = readLine().split(" ").map(_.toLong)
+  def random(n: Long) = {
+    var square = (n*n).toString
+    while (square.length < 8) square = "0" + square
+    square.drop(2).take(4).toLong
+  }
+  def cycle(n: Long) = {
+    var current = n
+    var items = Set[Long]()
+    while (!items(current)) {
+      items += current
+      current = random(current)
+    }
+    items.size
+  }
+  val cycles = numbers.map(cycle)
+  println(cycles.mkString(" "))
 }
