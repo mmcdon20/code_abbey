26d283943f35e3c8c4484f6151ec80477701a256
mmcdon20 <mmcdon20@live.com>
2014-05-25 23:35:22 -0500

    updated solution to problem 87

--- src/main/scala/Abbey087.scala
+++ src/main/scala/Abbey087.scala
@@ -1,26 +1,18 @@
 object Abbey087 extends App {
   val n = readInt()
   val numbers = readLine().split(" ").map(_.toInt).toList
-  val tree = Fork(numbers.head)
+  val tree = Node(numbers.head)
   numbers.tail.foreach(tree.insert)
   println(tree)
 }
 
-abstract class Tree {
-  def insert(n: Int): Unit
-}
-
-case class Fork(item: Int, var left: Tree = Leaf(), var right: Tree = Leaf()) extends Tree {
-  override def toString = s"($left,$item,$right)"
-  def insert(n: Int) = (left,right) match {
-    case (Leaf(),_) if n < item => left = Fork(n)
-    case (_,Leaf()) if n > item => right = Fork(n)
-    case _          if n < item => left.insert(n)
-    case _          if n > item => right.insert(n)
+case class Node(item: Int, var left: Node = null, var right: Node = null) {
+  private def sub(n: Node) = if (n == null) "-" else n
+  override def toString = s"(${sub(left)},$item,${sub(right)})"
+  def insert(n: Int): Unit = (left,right) match {
+    case (null,_) if n < item => left = Node(n)
+    case (_,null) if n > item => right = Node(n)
+    case (_,_)    if n < item => left.insert(n)
+    case (_,_)    if n > item => right.insert(n)
   }
-}
-
-case class Leaf() extends Tree {
-  override def toString = "-"
-  def insert(n: Int) = ()
 }
